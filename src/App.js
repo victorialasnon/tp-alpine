ea2ccb360b9d7a38089e5214aef7447a49ab843a
spagbol <38818825+pozorfluo@users.noreply.github.com>
2020-08-24 21:38:38 +0200

    Test drive react-router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
-import {useSelector} from "react-redux";
+import { useSelector } from 'react-redux';
+import routes from './routes';
 
 import {
   Version,
@@ -12,23 +13,37 @@ import {
   Summary,
 } from './Components/Placeholders';
 
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
 function App() {
   const test = useSelector((state) => state.test);
 
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          redux test : {test}
-        </p>
-
-        <Version />
+        <p>redux test : {test}</p>
+        <Router>
+        {routes.map(({ path, component }, key) => {
+          return (
+            <Link to={path} key={key}>
+              {path}
+            </Link>
+          );
+        })}
+          <Switch>
+            {routes.map(({ path, component }, key) => (
+              <Route exact path={path} key={key} render={component} />
+            ))}
+            ;
+          </Switch>
+        </Router>
+        {/* <Version />
         <Color />
         <Rims />
         <Upholstery />
         <Equipment />
         <Accessories />
-        <Summary />
+        <Summary /> */}
       </header>
     </div>
   );
