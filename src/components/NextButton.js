6f0e4b25c6d9017fb92e5a2961735a0d53dad685
victorialasnon <victoria.lasnon@laspote.net>
2020-08-25 16:57:41 +0200

    Add NextButton.js css & color App.css

---
+++ src/components/NextButton.js
@@ -0,0 +1,7 @@
+import React, {Component} from 'react';
+
+
+export const NextButton = (props) => {
+        return <button type="button" class="btn btn-outline-primary">
+            {props.text}</button>
+}
