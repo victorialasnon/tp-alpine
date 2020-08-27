ad17221e64e48975d7b6d63837ce2aba6a54e186
spagbol <38818825+pozorfluo@users.noreply.github.com>
2020-08-27 16:13:37 +0200

    Extract listItems function

--- src/App.js
+++ src/App.js
@@ -9,54 +9,57 @@ function App() {
   return (
     <div className="App">
       <Breadcrumbs />
-      <button
-        onClick={() => {
-          // dispatch({ type: 'CONFIG_SEND', transition: 'select' });
-          configMachine.send('select');
-        }}
-      >
-        select
-      </button>
-      <button
-        onClick={() => {
-          // dispatch({ type: 'CONFIG_SEND', transition: 'next' });
-          configMachine.send('next');
-        }}
-      >
-        next
-      </button>
-      <button
-        onClick={() => {
-          // dispatch({ type: 'CONFIG_SEND', transition: 'submit' });
-          configMachine.send('submit');
-        }}
-      >
-        submit
-      </button>
-      <button
-        onClick={() => {
-          // // dispatch({ type: 'CONFIG_SEND', transition: 'reset' });
-          configMachine.send('reset');
-        }}
-      >
-        reset
-      </button>
-      <button
-        onClick={() => {
-          // dispatch({ type: 'CONFIG_SEND', transition: 'confirm' });
-          configMachine.send('confirm');
-        }}
-      >
-        confirm
-      </button>
-      <button
-        onClick={() => {
-          // dispatch({ type: 'CONFIG_SEND', transition: 'cancel' });
-          configMachine.send('cancel');
-        }}
-      >
-        cancel
-      </button>
+      <div>
+        <hr />
+        <button
+          onClick={() => {
+            // dispatch({ type: 'CONFIG_SEND', transition: 'select' });
+            configMachine.send('select');
+          }}
+        >
+          select
+        </button>
+        <button
+          onClick={() => {
+            // dispatch({ type: 'CONFIG_SEND', transition: 'next' });
+            configMachine.send('next');
+          }}
+        >
+          next
+        </button>
+        <button
+          onClick={() => {
+            // dispatch({ type: 'CONFIG_SEND', transition: 'submit' });
+            configMachine.send('submit');
+          }}
+        >
+          submit
+        </button>
+        <button
+          onClick={() => {
+            // // dispatch({ type: 'CONFIG_SEND', transition: 'reset' });
+            configMachine.send('reset');
+          }}
+        >
+          reset
+        </button>
+        <button
+          onClick={() => {
+            // dispatch({ type: 'CONFIG_SEND', transition: 'confirm' });
+            configMachine.send('confirm');
+          }}
+        >
+          confirm
+        </button>
+        <button
+          onClick={() => {
+            // dispatch({ type: 'CONFIG_SEND', transition: 'cancel' });
+            configMachine.send('cancel');
+          }}
+        >
+          cancel
+        </button>
+      </div>
     </div>
   );
 }
